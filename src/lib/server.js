c936953421b175a3fc8d03a49798d2220291a0cd
Ben Lovy <ben@deciduously.com>
2020-05-09 16:36:52 -0400

    Switch prefix in prod

--- src/lib/server.js
+++ src/lib/server.js
@@ -1,15 +1,16 @@
 class Server {
     constructor(host, port) {
         this.host = host;
-        this.port = port;
-        console.log(`Using API server ${host}:${port}`);
+        this.port = port || "";
+        console.log(`Using API server ${host}`);
     }
     url() {
-        return `http://${this.host}:${this.port}/`
+        const prefix = process.env.NODE_ENV === 'production' ? 'https' : 'http'
+        return `${prefix}://${this.host}:${this.port}/`
     }
     rollEndpoint() {
         return `${this.url()}roll/`
     }
 }
 
-export const APIServer = new Server(process.env.API_HOST, process.env.API_PORT);
+export const APIServer = new Server(process.env.API_HOST, process.env.API_HOST);
